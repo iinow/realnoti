1c73003fcfcc7fccdc0e6be4e91991af350b231a
iinow <iinow@naver.com>
2018-10-03 23:22:44 +0900

    데이터 바인딩 추가

--- src/renderer/Login.jsx
+++ src/renderer/Login.jsx
@@ -12,25 +12,52 @@ const SIGNUP_LINK_STYLE = {
 }
 
 class Login extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            id : '',
+            pw : ''
+        }
+    }
+    // state = {
+        
+    // }
+
+    handleIdChange = (e) => {
+        const v = e.target.value
+        this.setState({id:v})
+    }
+
+    handlePwChange = (e) => {
+        const v = e.target.value
+        this.setState({pw:v})
+    }
+
     render() {
         return (
             <form style={FORM_STYLE}>
                 <div className="field">
                     <label className="label">아이디</label>
                     <div className="control">
-                        <input className="input" type="text" placeholder=""/>
+                        <input className="input" type="text" placeholder="" 
+                            value={this.state.id}
+                            onChange={this.handleIdChange}/>
                     </div>
                 </div>
                 
                 <div className="field">
                     <label className="label">비밀번호</label>
                     <div className="control">
-                        <input className="input" type="text" placeholder=""/>
+                        <input className="input" type="text" placeholder="" 
+                            value={this.state.pw}
+                            onChange={this.handlePwChange}/>
                     </div>
                 </div>
 
                 <div className="field">
-                    <button className="button">login</button>
+                    <button className="button" onClick={(e)=>{
+                        alert('클릭' + this.state.id+', '+this.state.pw)
+                    }}>login</button>
                 </div>
             </form>
         );
