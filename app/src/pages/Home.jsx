128555910a9134133122a04eb619bf88a41a48e9
sscu <diseum@gmail.com>
2018-10-10 16:21:13 +0900

    axios 기능 추가

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,11 +1,40 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import Api from '../util/Api'
 
 class Home extends Component {
+    state = {
+        url: '',
+        param: '',
+        res: ''
+    }
+
+    handleURLChange = (e) => {
+        const v = e.target.value
+        this.setState({ url: v })
+    }
+
+    handlePostClick = async (e) => {
+        const v = await Api.post(this.state.url, {
+            "userid":"ems@wooam.com",
+            "password":"1111"
+        })
+        console.log(v.data)
+        this.setState({ res: JSON.stringify(v.data) })
+        
+        alert(this.state.url)
+    }
     render() {
         return (
             <div>
                 Home Hello
+                <input onChange={this.handleURLChange}></input>
+                <button onClick={this.handlePostClick}>
+                    전송
+                </button>
+                <label>
+                    {this.state.res}
+                </label>
             </div>
         );
     }
