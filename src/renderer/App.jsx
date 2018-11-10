28c7b163b166df5372dd94c4422972852872a465
iinow <iinow@naver.com>
2018-11-10 20:21:14 +0900

    init commit

---
+++ src/renderer/App.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <button className="button is-primary">하인</button>
+            </div>
+        );
+    }
+}
+
+App.propTypes = {
+
+};
+
+export default App;
---
+++ src/renderer/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+// const app = () => {
+//     return (
+//         <div>
+//             <button>??</button>
+//         </div>
+//     )
+// }
+
+ReactDOM.render(<App/>, document.getElementById('root'))
